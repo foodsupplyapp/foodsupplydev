c56089b1e414bb5f8038940d757d4c8199c5ee8e
DESKTOP-6MEV34F\Shubham <mishrashubhu2@gmail.com>
2021-03-22 21:34:25 +0530

    Intitializing Project Files

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>
+    Welcome to nuxt
+  </h1>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
---
+++ pages/user/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>
+      <span>
+          Hey vue
+      </span>
+      
+  </h2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
